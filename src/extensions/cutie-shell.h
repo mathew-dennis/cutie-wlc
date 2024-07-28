bf76c29052ca1ba6221958c3d5a1457961488bed
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2024-07-28 21:36:44 +0530

    HardwareButtons: add enum to expose newly added keys

--- src/extensions/cutie-shell.h
+++ src/extensions/cutie-shell.h
@@ -15,29 +15,36 @@ class CutieShell : public QWaylandCompositorExtensionTemplate<CutieShell>,
 		   public QtWaylandServer::cutie_shell_private
 
 {
-	Q_OBJECT
-    public:
-	enum SpecialKey : uint32_t { POWER_PRESS = 0, POWER_RELEASE = 1 };
+    Q_OBJECT
+public:
+    enum SpecialKey : uint32_t {
+        POWER_PRESS = 0,
+        POWER_RELEASE = 1,
+        VOLUME_UP_PRESS = 2,
+        VOLUME_UP_RELEASE = 3,
+        VOLUME_DOWN_PRESS = 4,
+        VOLUME_DOWN_RELEASE = 5
+    };
 
-	CutieShell(CwlCompositor *compositor);
-	void initialize() override;
+    CutieShell(CwlCompositor *compositor);
+    void initialize() override;
 
-    signals:
+signals:
 
-    public Q_SLOTS:
-	void onBlurChanged(double blur);
-	void onSpecialKey(SpecialKey key);
-	void onThumbnailDamage(CwlView *view);
+public Q_SLOTS:
+    void onBlurChanged(double blur);
+    void onSpecialKey(SpecialKey key);
+    void onThumbnailDamage(CwlView *view);
 
-    protected:
+protected:
 	void cutie_shell_private_exec_app(Resource *resource,
 					  const QString &path) override;
 	void cutie_shell_private_get_thumbnail(
 		Resource *resource, uint32_t id,
 		struct ::wl_resource *toplevel) override;
 
-    private:
-	CwlCompositor *m_compositor;
+private:
+    CwlCompositor *m_compositor;
 };
 
 #endif //CUTIE_SHELL
